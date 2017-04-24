ba4fa39dffb55a0a79dbf556e2b9bdaabdbede04
Fahrizal Septrianto <fahrizalseptrianto@gmail.com>
2017-04-24 20:37:27 +0700

    initial commit for decision support system

---
+++ src/main/java/com/spk/ProfileMatchingApplication.java
@@ -0,0 +1,12 @@
+package com.spk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileMatchingApplication.class, args);
+	}
+}
